96d3d1f2dc8185010a507559aed616c741d9b1dd
Dennis Kawakami <denniskawakami@gmail.com>
2021-04-03 16:25:26 -0300

    commit inicial

---
+++ src/components/imageCards.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+function ImageCards({ image }) {
+  const tags = image.tags.split(",");
+
+  return (
+    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+      <img src={image.webformatURL} className="w-full" />
+      <div className="px-6 py-4">
+        <div className="font-bold text-purple-500 text-xl mb-2">
+          Photo by {image.user}
+        </div>
+        <ul>
+          <li>
+            <strong>Views: </strong>
+            {image.views}
+          </li>
+          <li>
+            <strong>Downloads: </strong>
+            {image.downloads}
+          </li>
+          <li>
+            <strong>Likes: </strong>
+            {image.likes}
+          </li>
+        </ul>
+      </div>
+      <div className="px-6 py-4">
+        {tags.map((tag, index) => (
+          <span
+            key={index}
+            className="inline-block bg-gray-200 rounded-full px-3 py-2 
+          text-sm font-smibold text-gray-700 mr-2 mb-5"
+          >
+            #{tag}
+          </span>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default ImageCards;
